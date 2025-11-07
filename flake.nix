4fe0202181cba857fb596b7af209128c04631cc3
yuanwang <yuan.wang@workiva.com>
2025-11-06 18:18:54 -0600

    wip

--- flake.nix
+++ flake.nix
@@ -5,10 +5,7 @@
     haskell-flake.url = "github:srid/haskell-flake";
     treefmt-nix.url = "github:numtide/treefmt-nix";
     flake-root.url = "github:srid/flake-root";
-
-    rust-overlay.url = "github:oxalica/rust-overlay";
     treefmt-nix.inputs.nixpkgs.follows = "nixpkgs";
-
     pre-commit = {
       url = "github:cachix/pre-commit-hooks.nix";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -24,12 +21,6 @@
         inputs.flake-root.flakeModule
       ];
       perSystem = { self', system, lib, config, pkgs, ... }: {
-        _module.args.pkgs = import inputs.nixpkgs {
-          inherit system;
-          overlays = [
-            inputs.rust-overlay.overlays.default
-          ];
-        };
         haskellProjects.default = {
           settings = { };
           # overrides = self: super: { };
@@ -41,7 +32,6 @@
         treefmt.config = {
           inherit (config.flake-root) projectRootFile;
           package = pkgs.treefmt;
-
           programs.ormolu.enable = true;
           programs.nixpkgs-fmt.enable = true;
           programs.cabal-fmt.enable = true;
@@ -56,49 +46,18 @@
           };
         };
         pre-commit.settings.hooks.treefmt.enable = true;
-        # # Dev shell scripts.
-        # mission-control.scripts = {
-        #   docs = {
-        #     description = "Start Hoogle server for project dependencies";
-        #     exec = ''
-        #       echo http://127.0.0.1:8888
-        #       hoogle serve -p 8888 --local
-        #     '';
-        #     category = "Dev Tools";
-        #   };
-        #   repl = {
-        #     description = "Start the cabal repl";
-        #     exec = ''
-        #       cabal repl "$@"
-        #     '';
-        #     category = "Dev Tools";
-        #   };
-        #   fmt = {
-        #     description = "Format the source tree";
-        #     exec = "${lib.getExe config.treefmt.build.wrapper}";
-        #     category = "Dev Tools ";
-        #   };
-        #   run = {
-        #     description = "Run the project with ghcid auto-recompile";
-        #     exec = ''
-        #       ghcid -c "cabal repl exe:haskell-template" --warnings -T :main
-        #     '';
-        #     category = "Primary";
-        #   };
-        # };
-
         # Default shell.
         devShells.default = pkgs.mkShell {
           inputsFrom = [
-
             config.pre-commit.devShell
             config.treefmt.build.devShell
             config.haskellProjects.default.outputs.devShell
           ];
           buildInputs = [
+            pkgs.ante
           ];
         };
-        packages.default = config.packages.aoc;
+        packages.default = config.packages.ec;
       };
     };
 }
