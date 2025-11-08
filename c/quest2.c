2d70b29c4b88de3e7e43317bb61af30edb84ea9f
yuanw <me@yuanwang.ca>
2025-11-08 08:32:56 -0600

    start quest 2

---
+++ c/quest2.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#define MAX_LINE_LENGTH 1024
+#define MAX_STRINGS 100
+#define MAX_INSTRUCTIONS 1000
+
+
+typedef struct {
+    int x;
+    int y;
+} Complex;
+
+
+int main() {
+    FILE *file = fopen("data/2025/quest1-part3.txt", "r");
+    if (file == NULL) {
+        perror("Error opening file");
+        return 1;
+    }
+
+    fclose(file);
+    return 0;
+}
